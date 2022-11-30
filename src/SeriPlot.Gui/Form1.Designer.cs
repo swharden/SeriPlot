1b260396c4fea9d4bfa009695bdccd4a65fee7f9
Scott W Harden <swharden@gmail.com>
2022-11-29 21:49:30 -0500

    SeriPlot 0.2: MVP

--- src/SeriPlot.Gui/Form1.Designer.cs
+++ src/SeriPlot.Gui/Form1.Designer.cs
@@ -28,11 +28,132 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+            this.components = new System.ComponentModel.Container();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbOpen = new System.Windows.Forms.CheckBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLastLine = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(12, 40);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(93, 23);
+            this.cbPort.TabIndex = 0;
+            this.cbPort.Text = "COM42";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cbOpen
+            // 
+            this.cbOpen.AutoSize = true;
+            this.cbOpen.Location = new System.Drawing.Point(240, 42);
+            this.cbOpen.Name = "cbOpen";
+            this.cbOpen.Size = new System.Drawing.Size(55, 19);
+            this.cbOpen.TabIndex = 3;
+            this.cbOpen.Text = "Open";
+            this.cbOpen.UseVisualStyleBackColor = true;
+            this.cbOpen.CheckedChanged += new System.EventHandler(this.cbOpen_CheckedChanged);
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Location = new System.Drawing.Point(173, 40);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(61, 23);
+            this.cbBaud.TabIndex = 4;
+            this.cbBaud.Text = "115200";
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formsPlot1.Location = new System.Drawing.Point(13, 69);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(774, 445);
+            this.formsPlot1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Baud";
+            // 
+            // lblLastLine
+            // 
+            this.lblLastLine.AutoSize = true;
+            this.lblLastLine.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLastLine.Location = new System.Drawing.Point(326, 44);
+            this.lblLastLine.Name = "lblLastLine";
+            this.lblLastLine.Size = new System.Drawing.Size(77, 14);
+            this.lblLastLine.TabIndex = 8;
+            this.lblLastLine.Text = "no data...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.lblLastLine);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbOpen);
+            this.Controls.Add(this.cbBaud);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.cbPort);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SeriPlot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
+
+    private ComboBox cbPort;
+    private Button button1;
+    private CheckBox cbOpen;
+    private ScottPlot.FormsPlot formsPlot1;
+    private ComboBox cbBaud;
+    private Label label1;
+    private Label label2;
+    private Label lblLastLine;
+    private System.Windows.Forms.Timer timer1;
 }
--- src/SeriPlot.Gui/Form1.cs
+++ src/SeriPlot.Gui/Form1.cs
@@ -1,9 +1,91 @@
+using ScottPlot.Plottable;
+using System.IO.Ports;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace SeriPlot.Gui;
 
 public partial class Form1 : Form
 {
+    SerialPort? Ser = null;
+    static string LastLine = string.Empty;
+    static readonly double[] Data = new double[2_000];
+    readonly ScottPlot.Plottable.VLine VLine;
+    static int NextDataIndex = 0;
+
     public Form1()
     {
         InitializeComponent();
+
+        cbBaud.Items.Add(115200);
+        cbBaud.SelectedIndex = 0;
+
+        formsPlot1.Plot.AddSignal(Data);
+        VLine = formsPlot1.Plot.AddVerticalLine(0, Color.Red, width: 2);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        ReScan();
+    }
+
+    private void ReScan()
+    {
+        cbPort.Items.Clear();
+        cbPort.Items.AddRange(SerialPort.GetPortNames());
+
+        if (cbPort.Items.Count > 0)
+            cbPort.SelectedIndex = cbPort.Items.Count - 1;
+    }
+
+    private void CloseSerialPort()
+    {
+        if (Ser is not null)
+        {
+            Ser.Close();
+            Ser.Dispose();
+            LastLine = "Disconnected";
+        }
+    }
+
+    private void OpenSerialPort()
+    {
+        LastLine = "Connecting...";
+        Application.DoEvents();
+        Ser = new SerialPort(cbPort.Text, int.Parse(cbBaud.Text));
+        Ser.Open();
+        Ser.ReadLine();
+        Ser.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+    }
+
+    private void cbOpen_CheckedChanged(object sender, EventArgs e)
+    {
+        if (cbOpen.Checked)
+            OpenSerialPort();
+        else
+            CloseSerialPort();
+    }
+
+    private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort sp = (SerialPort)sender;
+        string lastLine = sp.ReadLine().Trim();
+        LastLine = lastLine;
+        string[] parts = lastLine.Split(",");
+        try
+        {
+            Data[NextDataIndex % Data.Length] = double.Parse(parts[0]);
+            NextDataIndex += 1;
+        }
+        catch
+        {
+            System.Diagnostics.Debug.WriteLine($"PARSING ERROR: {LastLine}");
+        }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        lblLastLine.Text = LastLine;
+        VLine.X = NextDataIndex % Data.Length;
+        formsPlot1.Refresh();
     }
 }
